075b312090f1c3ee30ef8c58c3716e588823821b
yo <hey@gmail.com>
2023-08-03 15:01:37 +0530

    add black folder

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 // const CONNECTION_URL = 'mongodb+srv://manish:Manish2002ar@memory.tchl5cg.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT|| 5000;
+const PORT = process.env.PORT || 5000;
 
 // mongodb+srv://manish:<password>@atlascluster.uqscenz.mongodb.net/?retryWrites=true&w=majority
 // const CONNECTION_URL ='mongodb+srv://manish:Manish2002ar@cluster0.msfhyqp.mongodb.net/?retryWrites=true&w=majority'
